54d38be282cb06840e6ae304f87088393974f12d
Jose Ricardo Torres Mendez <josertorresm1996@gmail.com>
2020-10-01 02:14:16 -0600

    improved data extracting from setup

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-document.getElementById("chriss").addEventListener("click", function( e ) {
+document.getElementById("submit").addEventListener("click", function( e ) {
   e.preventDefault();
-  var e = document.getElementById("eventOptions");
+  
 
+  var initials = document.getElementById("initials").value;
+  if( initials.length < 1 ){
+  	alert("please input analyst initials");
+  	return;
+  }
+  window.location = "event.html";
 
   //var selection = e.options[e.selectedIndex].value;
   //console.log( selection );
   
 
   //alert("Not yet implemented");
-  //window.location = "finding.html";
 
 });
