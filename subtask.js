bf14078bd7547f9f5c8c023668663105f1eaba9e
ChrisAaren <chrisaaren01@gmail.com>
2020-10-27 21:09:00 -0600

    added adding a subtask interface

---
+++ subtask.js
@@ -0,0 +1,16 @@
+function addSubtask() {
+  var addSubtaskForm = document.getElementById("addSubtask");
+  if (addSubtaskForm.style.display === "none") {
+    addSubtaskForm.style.display = "block";
+  } else {
+    addSubtaskForm.style.display = "none";
+  }
+}
+
+
+function addFormAppear() {
+	var addSubtaskForm = document.getElementById("addSubtask");
+	if(addSubtaskForm.style.display === "none") {
+		addSubtaskForm.style.display = "block"
+	}
+}
