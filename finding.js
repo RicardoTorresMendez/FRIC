58c917d8b4ff14f0affaad95c92fa216af64b611
ChrisAaren <chrisaaren01@gmail.com>
2020-10-29 02:19:23 -0600

    updated finding UI and added an add form for finding

---
+++ finding.js
@@ -0,0 +1,16 @@
+function addFinding() {
+  var addFindingForm = document.getElementById("addFinding");
+  if (addSubtaskForm.style.display === "none") {
+    addSubtaskForm.style.display = "block";
+  } else {
+    addSubtaskForm.style.display = "none";
+  }
+}
+
+
+function addFormAppear() {
+	var addFindingForm = document.getElementById("addFinding");
+	if(addFindingForm.style.display === "none") {
+		addFindingForm.style.display = "block"
+	}
+}
