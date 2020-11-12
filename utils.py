d0fcb6f7e72fe31a1ba9084b436b41ff99021572
ctiscareno <ctisca1998@gmail.com>
2020-11-12 00:08:09 -0700

    added template and excel sheet

--- utils.py
+++ utils.py
@@ -2,14 +2,14 @@ from docxtpl import DocxTemplate
 import jinja2
 import pandas as pd
 
+def gen_final_report():
+    doc = DocxTemplate('CVPA_REPORT_template.docx')
 
-doc = DocxTemplate('CVPA_REPORT_template.docx')
+    note_data = pd.read_excel('FinalReportVar.xlsx')
 
-note_data = pd.read_excel('FinalReportVar.xlsx')
-
-context = dict(zip(note_data['var'], note_data['value']))
-doc.render(context)
-doc.save('FinalReport3.docx')
+    context = dict(zip(note_data['var'], note_data['value']))
+    doc.render(context)
+    doc.save('FinalReport3.docx')
 
 if __name__ == "__main__": 
     print("Executed when invoked directly")
