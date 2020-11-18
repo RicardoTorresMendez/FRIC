aeff8d4b72ebd722f4aaaabbf5f2c1df8dbdd5fe
Jose Ricardo Torres Mendez <josertorresm1996@gmail.com>
2020-11-17 20:05:18 -0700

    a few details left out

--- sync.js
+++ sync.js
@@ -2,6 +2,16 @@ document.getElementById("sync").addEventListener("click", function( e ){
 
 	var IP = document.getElementById("IPAddress").value;
 
+	fetch( IP + "/syncRequest.php", {
+        method: 'POST',
+        body: formData
+  }).then(response => response.json())
+  .then(function(response){
+
+  	
+
+	});
+
 }
 
 
